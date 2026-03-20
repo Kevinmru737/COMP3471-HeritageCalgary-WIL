f09371198b37169ecfabfcc2bbb09a55b48b61a5
Kevin <koh705@mtroyal.ca>
2026-03-19 19:33:59 -0600

    app spelling

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def load_model():
 results = load_data()
 model = load_model()
 
-st.title("Calgary Heritage Building Designator")
+st.title("Calgary Heritage Designation Machine Learning Experiment")
 
 df = pd.read_csv("trimmed-data-1.csv")
 
